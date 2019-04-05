e9874c4ded817743b561cedb0f875c8a2af6fada
Edwin Njeru <mailnjeru@gmail.com>
2019-04-05 09:27:51 +0300

    Add note to the RiskClass enum

--- src/main/java/io/github/ifris/loans/domain/enumeration/RiskClass.java
+++ src/main/java/io/github/ifris/loans/domain/enumeration/RiskClass.java
@@ -2,6 +2,9 @@ package io.github.ifris.loans.domain.enumeration;
 
 /**
  * The RiskClass enumeration.
+ *
+ * This class contains the classifications defined by the Central Bank of Kenya in the
+ * prudential guidelines, in accordance with implementation of the IAS39.
  */
 public enum RiskClass {
     NORMAL, WATCH, SUBSTANDARD, DOUBTFUL, LOSS
